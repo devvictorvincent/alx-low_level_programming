e90b9e75939aaf63203092377620ba3077b022e1
Vincent Victor <dev.victorvincent@gmail.com>
2023-03-18 00:36:50 -0700

    0-9 count using int and putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		putchar('0' + i);
+	putchar('\n');
+	return (0);
+}
