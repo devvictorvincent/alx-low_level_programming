0c577479119dff821c6438baefb7a7131b989511
Vincent Victor <dev.victorvincent@gmail.com>
2023-03-17 23:53:17 -0700

    excluding e and q from my abcd

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'e' && i != 'q')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
