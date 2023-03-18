5f2a3ec7df3311e188fab1b9f9b679b96de3d5fd
Vincent Victor <dev.victorvincent@gmail.com>
2023-03-17 19:53:06 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - the main program entry point
+ * return  :always 0 (meaning success)
+ * */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
