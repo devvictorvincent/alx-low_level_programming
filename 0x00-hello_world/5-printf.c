52c3bfdf2093bcfc41f96b68c93a8db6098c9bb8
Vincent Victor <dev.victorvincent@gmail.com>
2023-03-20 16:58:13 -0700

    c printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("\n");
+	return (0);
+}
