566d3c05459ee54a59d9d6760edb739aa74dafb0
Vincent Victor <dev.victorvincent@gmail.com>
2023-03-22 09:30:30 -0700

    starting nested loops and functions with putchar task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
