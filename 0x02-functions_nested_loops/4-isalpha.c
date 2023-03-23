f46dc1987175c6896dc6c2d0e0e15ec0242b5d9a
Vincent Victor <dev.victorvincent@gmail.com>
2023-03-22 17:49:47 -0700

    all alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * _isalpha - main function
+ *
+ * @c: parameters for looking at all alphabets
+ *
+ * _putchar: prints alphabets
+ *
+ * Return: always 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return(9);
+}
