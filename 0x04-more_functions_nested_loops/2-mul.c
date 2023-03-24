cab415300ab8ffc24362f3ef45dbf0453be06b93
Vincent Victor <dev.victorvincent@gmail.com>
2023-03-24 16:27:35 -0700

    multitplcation of two int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * mul - main function
+ *
+ * @a: first parameter
+ *
+ * @b: second parameter number
+ *
+ * Return: 0 if not upper case, if upper case
+ */
+
+int mul(int a, int b)
+{
+	int n;
+
+	n = a * b;
+	return (n);
+}
