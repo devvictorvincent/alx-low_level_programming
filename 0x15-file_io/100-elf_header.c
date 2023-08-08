efa80ec99c39f6a3c647701c1d2f246265ec379e
vincent Victor <dev.victorvncent@gmail.com>
2023-08-07 19:09:48 -0700

    quick fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -21,13 +21,13 @@ void close_elf(int elf);
  * check_elf - Checks if a file is an ELF file.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
- * Description: If the file is not an ELF file - exit code 98.
+i * Description: If the file is not an ELF file - exit code 98.
  */
 void check_elf(unsigned char *e_ident)
 {
 	int i;
 
-	for (i 0; i < 4; i++)
+	for (i  = 0; i < 4; i++)
 	{
 		if (e_ident[i] != 127 &&
 		    e_ident[i] != 'E' &&
